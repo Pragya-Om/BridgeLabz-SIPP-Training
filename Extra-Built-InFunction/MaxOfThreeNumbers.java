e578a18af63e50b678c8952a0e41f6c5deca7402
Pragya-Om <pragyaom97@gmail.com>
2025-06-20 09:07:40 +0530

    [Pragya] MaxOfThreeNumbers program

---
+++ Extra-Built-InFunction/MaxOfThreeNumbers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MaxOfThreeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = getUserInput(scanner, "Enter the first number: ");
+        int num2 = getUserInput(scanner, "Enter the second number: ");
+        int num3 = getUserInput(scanner, "Enter the third number: ");
+
+        int max = findMaximum(num1, num2, num3);
+
+        System.out.println("The maximum of the three numbers is: " + max);
+        scanner.close();
+    }
+
+    public static int getUserInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+
+    public static int findMaximum(int a, int b, int c) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
+} 
